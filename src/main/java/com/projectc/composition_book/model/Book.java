6e5774396454451b56c1541f0b6df506df4ae4af
Anshul0707 <anshul.markwade786@gmail.com>
2023-06-09 12:39:08 +0530

    models

---
+++ src/main/java/com/projectc/composition_book/model/Book.java
@@ -0,0 +1,12 @@
+package com.projectc.composition_book.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Book {
+    private String title;
+    private List<Author> authors;
+    private Publisher publisher;
+}
